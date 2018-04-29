32ac23d33d490be0d9f4a323824022ec463b1a57
Daksha Patel <dakshapatel100@gmail.com>
2018-04-29 15:42:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,6 +10,19 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+def play(board)
+  while !over?(board)
+   turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+end
+
+
 
 def display_board(board)
    puts " #{board[0]} | #{board[1]} | #{board[2]} "
